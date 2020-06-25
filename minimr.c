35d7fdd781e768c4198c421b75384476b84e0e09
philip <me@filou.se>
2020-06-25 15:55:30 +0200

    untested code

---
+++ minimr.c
@@ -0,0 +1,432 @@
+//
+// Created by Philip Tschiemer on 25.06.20.
+//
+
+#include "minimr.h"
+
+void minimr_dns_normalize_name(struct minimr_dns_rr * rr)
+{
+    ASSERT( rr != NULL );
+
+    for(uint16_t i = 0; i < rr->name_length; i++){
+        if (rr->name[i] == '.'){
+            rr->name[i] = '\0';
+        }
+    }
+}
+
+uint8_t minimr_dns_extract_query_stat(struct minimr_dns_query_stat * stat, uint8_t * msg, uint16_t * pos, uint16_t msglen)
+{
+    uint16_t p = *pos;
+
+    // minlen = QNAME(3)[= <len> <name> NUL] QTYPE(2) UNICAST/QCLASS(2)
+    if (p + 7 >= msglen){
+        return MINIMR_NOT_OK;
+    }
+
+    for(; p < msglen && msg[p] != '\0'; p++){
+        // just looking for the end of qname
+        // note: it's not checked wether the qname is correctly formatted
+
+        // fail if the name of a question is compressed.
+        if ( (msg[p] & MINIMR_DNS_COMPRESSED_NAME) == MINIMR_DNS_COMPRESSED_NAME){
+            return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+        }
+    }
+
+    // simple sanity check
+    // did not go beyond msg
+    // read at least 3 bytes?
+    // remaining minlen = QTYPE(2) UNICAST/QCLASS(2)
+    if (p >= msglen || p - *pos < 3 || p + 4 > msglen){
+        return MINIMR_NOT_OK;
+    }
+
+    stat->name_offset = *pos;
+    stat->name_length = p - *pos;
+
+    stat->type = msg[p] << 8;
+    stat->type |= msg[p++];
+
+    stat->unicast_class = msg[p++] << 8;
+    stat->unicast_class |= msg[p++];
+
+    *pos = p;
+
+    return MINIMR_OK;
+}
+
+uint8_t minimr_dns_extract_rr_stat(struct minimr_dns_rr_stat * stat, uint8_t * msg, uint16_t *pos, uint16_t msglen)
+{
+    uint16_t p = *pos;
+
+    // minlen = QNAME(2) RRTYPE(2) CACHE/RRCLASS(2) TTL(4) RDLENGTH(2) + RDLENGTH
+    if (p + 12 >= msglen){
+        return MINIMR_NOT_OK;
+    }
+
+
+    if ( (msg[p] & MINIMR_DNS_COMPRESSED_NAME) == MINIMR_DNS_COMPRESSED_NAME){
+        stat->name_offset = (MINIMR_DNS_COMPRESSED_NAME_OFFSET & msg[p++]) << 8;
+        stat->name_offset |=  msg[p++];
+
+        uint16_t name_pos = stat->name_offset;
+
+        for(; name_pos < msglen && msg[name_pos] != '\0'; name_pos++){
+            // just looking for the end of rrname
+            // note: it's not checked wether the rrname is correctly formatted
+
+            // do not allow recursive compression
+            if ( (msg[name_pos] & MINIMR_DNS_COMPRESSED_NAME) == MINIMR_DNS_COMPRESSED_NAME){
+                return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+            }
+        }
+
+
+        // simple sanity check
+        // did not go beyond msg
+        // read at least 3 bytes?
+        if (name_pos >= msglen || name_pos - stat->name_offset < 3){
+            return MINIMR_NOT_OK;
+        }
+
+        stat->name_length = name_pos - stat->name_offset;
+
+    } else {
+        stat->name_offset = p;
+
+        for(; p < msglen && msg[p] != '\0'; p++){
+            // just looking for the end of rrname
+            // note: it's not checked wether the rrname is correctly formatted
+
+            // do not allow partial name compression
+            // fail if the name of a question is compressed.
+            if ( (msg[p] & MINIMR_DNS_COMPRESSED_NAME) == MINIMR_DNS_COMPRESSED_NAME){
+                return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+            }
+        }
+
+        // simple sanity check
+        // did not go beyond msg
+        // read at least 3 bytes?
+        if (p >= msglen || p - *pos < 3 ){
+            return MINIMR_NOT_OK;
+        }
+
+        stat->name_length = p - *pos;
+
+        p++;
+    }
+
+    // minlen = RRTYPE(2) CACHE/RRCLASS(2) TTL(4) RDLENGTH(2) + RDLENGTH
+    if (p + 10 >= msglen){
+        return MINIMR_NOT_OK;
+    }
+
+
+    stat->type = msg[p++] << 8;
+    stat->type |= msg[p++];
+
+    stat->cache_class = msg[p++] << 8;
+    stat->cache_class |= msg[p++];
+
+    stat->ttl = msg[p++] << 24;
+    stat->ttl |= msg[p++] << 16;
+    stat->ttl |= msg[p++] << 8;
+    stat->ttl |= msg[p++];
+
+    stat->dlength = msg[p++] << 8;
+    stat->dlength |= msg[p++];
+
+    if (p + stat->dlength >= msglen){
+        return MINIMR_NOT_OK;
+    }
+
+    stat->data_offset = p;
+
+    *pos = p + stat->dlength;
+
+    return MINIMR_OK;
+}
+
+
+uint8_t minimr_handle_msg(
+        uint8_t * msg, uint16_t msglen,
+        struct minimr_dns_query_stat qstats[], uint16_t nqstats,
+        struct minimr_dns_rr ** records, uint16_t nrecords,
+        uint8_t *outmsg, uint16_t * outmsglen, uint16_t outmsgmaxlen
+)
+{
+    ASSERT(msg != NULL);
+    ASSERT(qstats != NULL);
+    ASSERT(nqstats > 0);
+    ASSERT(records != NULL);
+    ASSERT(nrecords > 0);
+    ASSERT(outmsg != NULL);
+    ASSERT(outmsglen != NULL);
+    ASSERT(outmsgmaxlen > MINIMR_DNS_HDR_SIZE);
+
+
+    // ignore messages that are not long enough to even have a complete header
+    if (msglen < MINIMR_DNS_HDR_SIZE){
+        return MINIMR_IGNORE;
+    }
+
+    struct minimr_dns_hdr hdr;
+
+    // read header info
+    minimr_dns_ntoh_hdr(&hdr, msg);
+
+
+    // not a (standard) query? ignore
+    // or no questions? nothing to do!
+    if ( (hdr.flags[0] & MINIMR_DNS_HDR1_QR) != MINIMR_DNS_HDR1_QR_QUERY ||
+         (hdr.flags[0] & MINIMR_DNS_HDR1_OPCODE) != MINIMR_DNS_HDR1_OPCODE_QUERY ||
+         hdr.nquestions == 0){
+
+        return MINIMR_IGNORE;
+    }
+
+
+    uint16_t pos = MINIMR_DNS_HDR_SIZE;
+    uint16_t nq = 0;
+
+    // note all relevant questions for us
+    // stored in stats as 0 - nq
+    for(uint16_t iq = 0; iq < hdr.nquestions && nq < nqstats && pos < msglen; iq++){
+
+        uint8_t res = minimr_dns_extract_query_stat(&qstats[nq], msg, &pos, msglen);
+
+        // in case of a server fail, pass this along
+        if (res == MINIMR_DNS_HDR2_RCODE_SERVAIL) {
+            return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+        }
+
+        if (res != MINIMR_OK){
+            // we could respond that it was a faulty query..
+            return MINIMR_DNS_HDR2_RCODE_FORMERR;
+        }
+
+        for(uint16_t ir = 0; ir < nrecords; ir++){
+
+            if (qstats[nq].type != records[ir]->type) continue;
+
+            // unless ANY class was asked for check if classes match
+            if ((qstats[nq].unicast_class & MINIMR_DNS_QCLASS) != MINIMR_DNS_CLASS_ANY &&
+                    (qstats[nq].unicast_class & MINIMR_DNS_QCLASS) != (records[ir]->cache_class & MINIMR_DNS_RRCLASS) ) continue;
+
+            // if name lengths don't match, there's no point checking names
+            if (qstats[nq].name_length != records[ir]->name_length) continue;
+
+            // pretty much a reverse memcmp of the name
+            uint8_t found = 0;
+            for(int32_t i = qstats[nq].name_length - 1; found == 0 && i > 0; i--){
+                uint8_t * qname = &msg[qstats[nq].name_offset];
+                if (qname[i] != records[ir]->name[i]){
+                    found = 1;
+                }
+            }
+
+            if (found == 0) continue;
+
+
+            // so it's a match and we might consider responding
+            // but let's remember this question and the matching record and let's go to the next question
+
+            qstats[nq].relevant = 1;
+            qstats[nq].ir = ir;
+
+            nq++;
+
+            break;
+        }
+
+    }
+
+    // msg seems to be faulty, stop processing
+    if (pos >= msglen){
+        return MINIMR_DNS_HDR2_RCODE_FORMERR;
+    }
+
+    // no questions we need to answer
+    if (nq == 0){
+        return MINIMR_IGNORE;
+    }
+
+    // note how many questions we actually have to answer
+    // (can change after checking the known answers)
+    uint16_t remaining_nq = nq;
+
+    // now check all known answers
+    if (hdr.nanswers > 0){
+
+        for(uint16_t ia = 0; ia < hdr.nanswers && nq < nqstats && pos < msglen; ia++){
+
+            struct minimr_dns_rr_stat rstat;
+
+            uint8_t res = minimr_dns_extract_rr_stat(&rstat, msg, &pos, msglen);
+
+            // in case of a server fail, pass this along
+            if (res == MINIMR_DNS_HDR2_RCODE_SERVAIL) {
+                return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+            }
+
+            if (res != MINIMR_OK){
+                // we could respond that it was a faulty query..
+                return MINIMR_DNS_HDR2_RCODE_FORMERR;
+            }
+
+            // check if the known answer relates to any of the relevant questions
+
+            for(uint16_t iq = 0; iq < nq; iq++){
+
+                // same type?
+                if (rstat.type != qstats[iq].type) continue;
+
+                // same class?
+                if ((rstat.cache_class & MINIMR_DNS_RRCLASS) != (qstats[iq].unicast_class & MINIMR_DNS_QCLASS) ) continue;
+
+
+                // only check name if name offsets do not match
+                if (rstat.name_offset != qstats[iq].name_offset){
+
+                    // if name lengths don't match, there's no point checking names
+                    if (rstat.name_length != qstats[iq].name_length) continue;
+
+                    uint8_t found = 0;
+
+                    // pretty much a reverse memcmp of the name
+                    for(int32_t i = rstat.name_length - 1; found == 0 && i > 0; i--){
+                        uint8_t * rname = &msg[rstat.name_offset];
+                        uint8_t * qname = &msg[qstats[iq].name_offset];
+                        if (qname[i] != qname[i]){
+                            found = 1;
+                        }
+                    }
+
+                    if (found == 0) continue;
+                }
+
+                // so it's a match and we have to check wether it's up to date
+                struct minimr_dns_rr * rr = records[qstats[iq].ir];
+                if (rr->fun(minimr_dns_rr_fun_type_is_uptodate, rr, &rstat, msg) == MINIMR_UPTODATE){
+                    qstats[iq].relevant = 0;
+                    remaining_nq--;
+                }
+
+                break;
+            }
+
+        }
+
+        // msg seems to be faulty, stop processing
+        if (pos >= msglen){
+            return MINIMR_DNS_HDR2_RCODE_FORMERR;
+        }
+    }
+
+    // oh, all our records are known already! time for a coffee
+    if (remaining_nq == 0){
+        return MINIMR_IGNORE;
+    }
+
+
+    // sanity check config
+    if (outmsgmaxlen <= MINIMR_DNS_HDR_SIZE){
+        return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+    }
+
+
+    uint16_t outlen = MINIMR_DNS_HDR_SIZE;
+
+    uint16_t nanswers = 0;
+
+    // add all normal answers RRs
+    for(uint16_t iq = 0; iq < nq; iq++){
+
+        // don't check questions that have become irrelevant
+        if (qstats[iq].relevant == 0){
+            continue;
+        }
+
+        struct minimr_dns_rr * rr = records[qstats[iq].ir];
+
+        uint16_t nrr = 0;
+
+        uint8_t res = rr->fun(minimr_dns_rr_fun_type_get_answer_rrs, rr, outmsg, &outlen, outmsgmaxlen, &nrr);
+
+        if (res != MINIMR_OK){
+            return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+        }
+
+        nanswers += nrr;
+    }
+
+    // add all authority RRs
+    uint16_t nauthrr = 0;
+    for(uint16_t iq = 0; iq < nq; iq++){
+
+        // don't check questions that have become irrelevant
+        if (qstats[iq].relevant == 0){
+            continue;
+        }
+
+        struct minimr_dns_rr * rr = records[qstats[iq].ir];
+
+        uint16_t nrr = 0;
+
+        uint8_t res = rr->fun(minimr_dns_rr_fun_type_get_authority_rrs, rr, outmsg, &outlen, outmsgmaxlen, &nrr);
+
+        if (res != MINIMR_OK){
+            return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+        }
+
+        nauthrr += nrr;
+    }
+
+    // add all additional RRs
+    uint16_t nextrarr = 0;
+    for(uint16_t iq = 0; iq < nq; iq++){
+
+        // don't check questions that have become irrelevant
+        if (qstats[iq].relevant == 0){
+            continue;
+        }
+
+        struct minimr_dns_rr * rr = records[qstats[iq].ir];
+
+        uint16_t nrr = 0;
+
+        uint8_t res = rr->fun(minimr_dns_rr_fun_type_get_authority_rrs, rr, outmsg, &outlen, outmsgmaxlen, &nrr);
+
+        if (res != MINIMR_OK){
+            return MINIMR_DNS_HDR2_RCODE_SERVAIL;
+        }
+
+        nextrarr += nrr;
+    }
+
+    // prepare outheader and out sanity check
+    struct minimr_dns_hdr outhdr;
+
+//    // memset outheader
+//    for(int i = 0; i < sizeof(outhdr); i++){
+//        *(uint8_t*)&outhdr = 0;
+//    }
+
+    // finalize header
+    outhdr.transaction_id = hdr.transaction_id; // likely to be 0x0000 ....
+
+    outhdr.flags[0] = MINIMR_DNS_HDR1_QR_REPLY | MINIMR_DNS_HDR1_AA;
+    outhdr.flags[1] = MINIMR_DNS_HDR2_RCODE_NOERROR;
+
+    outhdr.nquestions = 0;
+    outhdr.nanswers = nanswers;
+    outhdr.nauthrr = nauthrr;
+    outhdr.nextrarr = nextrarr;
+
+    // add header
+    minimr_dns_hton_hdr(outmsg, &outhdr);
+
+    return MINIMR_DNS_HDR2_RCODE_NOERROR;
+}
--- minimr.h
+++ minimr.h
@@ -1,14 +1,29 @@
 //
 // Created by Philip Tschiemer on 24.06.20.
 //
+// official definitions that most likely are not needed are just commented out
+//
 
 #ifndef MINIMR_DNS_MINIMR_DNS_H
 #define MINIMR_DNS_MINIMR_DNS_H
 
 #include <stdint.h>
 
-#define MINIMR_OK       0
-#define MINIMR_NOT_OK   1
+#ifndef ASSERT
+#define ASSERT(x)
+#endif
+
+#define MINIMR_IGNORE           0xff
+#define MINIMR_CONFIG_ERROR     0xfe
+#define MINIMR_BUFFER_OVERFLOW  0xfd
+
+
+#define MINIMR_OK           0
+#define MINIMR_NOT_OK       1
+
+#define MINIMR_UPTODATE         2
+#define MINIMR_NOT_UPTODATE     3
+
 
 #define MINIMR_DNS_HDR_SIZE 12
 
@@ -22,12 +37,15 @@
 #define MINIMR_DNS_HDR2_Z       0x70    // zeros (reserved)
 #define MINIMR_DNS_HDR2_RCODE   0x0F    // response code: NOERROR (0), FORMERR (1, format error), SERVAIL (2), NXDOMAIN ( 3, nonexistent domain)
 
+#define MINIMR_DNS_HDR1_QR_QUERY        0x00
+#define MINIMR_DNS_HDR1_QR_REPLY        0x80
+
 #define MINIMR_DNS_HDR1_OPCODE_QUERY    0x00    // standard query (0)
-#define MINIMR_DNS_HDR1_OPCODE_IQUERY   0x08    // inverse query (1)
-#define MINIMR_DNS_HDR1_OPCODE_STATUS   0x10    // server status request (2)
+//#define MINIMR_DNS_HDR1_OPCODE_IQUERY   0x08    // inverse query (1)
+//#define MINIMR_DNS_HDR1_OPCODE_STATUS   0x10    // server status request (2)
 #define MINIMR_DNS_HDR1_OPCODE_NOTIFY   0x20    // notify (4)
 #define MINIMR_DNS_HDR1_OPCODE_UPDATE   0x28    // update (5)
-#define MINIMR_DNS_HDR1_OPCODE_DSO      0x30    // DNS Stateful operations (6)
+//#define MINIMR_DNS_HDR1_OPCODE_DSO      0x30    // DNS Stateful operations (6)
 
 #define MINIMR_DNS_HDR2_RCODE_NOERROR   0   // ok (0)
 #define MINIMR_DNS_HDR2_RCODE_FORMERR   1   // format error (1)
@@ -39,16 +57,6 @@
 #define MINIMR_DNS_HDR2_RCODE_YXRRSET   7   // RR set exists when it should not
 #define MINIMR_DNS_HDR2_RCODE_NXRRSET   8   // RR set that should exist does not
 #define MINIMR_DNS_HDR2_RCODE_NOTAUTH   9   // not authorizezd / server not authoritaive for zone
-#define MINIMR_DNS_HDR2_RCODE_DSOTYPENI 10  // DSA-TYPE not implemented
-#define MINIMR_DNS_HDR2_RCODE_BADVERS   16  // bad opt version
-#define MINIMR_DNS_HDR2_RCODE_BADSIG    16  // TSIG signature failure
-#define MINIMR_DNS_HDR2_RCODE_BADKEY    17  // key not recognized
-#define MINIMR_DNS_HDR2_RCODE_BADTIME   18  // signature out of time window
-#define MINIMR_DNS_HDR2_RCODE_BADMODE   19  // bad TKEY mode
-#define MINIMR_DNS_HDR2_RCODE_BADNAME   20  // duplicate key name
-#define MINIMR_DNS_HDR2_RCODE_BAGALG    21  // algorithm not supported
-#define MINIMR_DNS_HDR2_RCODE_BADTRUNC  22  // bad truncation
-#define MINIMR_DNS_HDR2_RCODE_BADCOOKIE 23  // bad missing server cookie
 
 
 
@@ -64,101 +72,183 @@ struct minimr_dns_hdr {
 };
 
 #define MINIMR_DNS_UNICAST          0x8000  // unicast requested
-#define MINIMR_DNS_QCLASS           0x7FFF
+#define MINIMR_DNS_QCLASS           0x7FFF  // mask for qclass
 
-#define MINIMR_DNS_QCLASS_IN        0x0001
-#define MINIMR_DNS_QCLASS_CH        0x0003
-#define MINIMR_DNS_QCLASS_HS        0x0004
-#define MINIMR_DNS_QCLASS_NONE      0x00fe
-#define MINIMR_DNS_QCLASS_ANY       0x00ff
+#define MINIMR_DNS_CLASS_IN        0x0001
+//#define MINIMR_DNS_CLASS_CH        0x0003
+//#define MINIMR_DNS_CLASS_HS        0x0004
+//#define MINIMR_DNS_CLASS_NONE      0x00fe
+#define MINIMR_DNS_CLASS_ANY       0x00ff
 
-#define MINIMR_DNS_QTYPE_A           1
-#define MINIMR_DNS_QTYPE_AAAA        28
-#define MINIMR_DNS_QTYPE_PTR         12
-#define MINIMR_DNS_QTYPE_SRV         33
-#define MINIMR_DNS_QTYPE_TXT         16
+#define MINIMR_DNS_TYPE_A           1   // ipv4 addr
+#define MINIMR_DNS_TYPE_AAAA        28  // ipv6 addr
+#define MINIMR_DNS_TYPE_PTR         12  // generic ptr
+#define MINIMR_DNS_TYPE_SRV         33  // service
+#define MINIMR_DNS_TYPE_TXT         16  // options
 
-#define MINIMR_DNS_QTYPE_AFSDB       18
-#define MINIMR_DNS_QTYPE_APL         42
-#define MINIMR_DNS_QTYPE_CAA         257
-#define MINIMR_DNS_QTYPE_CDNSKEY     60
-#define MINIMR_DNS_QTYPE_CDS         59
-#define MINIMR_DNS_QTYPE_CERT        37
-#define MINIMR_DNS_QTYPE_CNAME       5
-#define MINIMR_DNS_QTYPE_CSYNC       62
-#define MINIMR_DNS_QTYPE_DHCID       49
-#define MINIMR_DNS_QTYPE_DLV         32769
-#define MINIMR_DNS_QTYPE_DNAME       39
-#define MINIMR_DNS_QTYPE_DNSKEY      48
-#define MINIMR_DNS_QTYPE_DS          43
-#define MINIMR_DNS_QTYPE_HINFO       13
-#define MINIMR_DNS_QTYPE_HIP         55
-#define MINIMR_DNS_QTYPE_IPSECKEY    45
-#define MINIMR_DNS_QTYPE_KEY         25
-#define MINIMR_DNS_QTYPE_KX          36
-#define MINIMR_DNS_QTYPE_LOC         29
-#define MINIMR_DNS_QTYPE_MX          15
-#define MINIMR_DNS_QTYPE_NAPTR       35
-#define MINIMR_DNS_QTYPE_NS          2
-#define MINIMR_DNS_QTYPE_NSEC        47
-#define MINIMR_DNS_QTYPE_NSEC3       50
-#define MINIMR_DNS_QTYPE_NSEC3PARAM  51
-#define MINIMR_DNS_QTYPE_OPENGPGKEY  61
-#define MINIMR_DNS_QTYPE_RRSIG       46
-#define MINIMR_DNS_QTYPE_RP          17
-#define MINIMR_DNS_QTYPE_SIG         24
-#define MINIMR_DNS_QTYPE_SMIMEA      53
-#define MINIMR_DNS_QTYPE_SOA         6
-#define MINIMR_DNS_QTYPE_SSHFP       44
-#define MINIMR_DNS_QTYPE_TA          32768
-#define MINIMR_DNS_QTYPE_TKEY        249
-#define MINIMR_DNS_QTYPE_TLSA        52
-#define MINIMR_DNS_QTYPE_TSIG        250
-#define MINIMR_DNS_QTYPE_URI         256
-#define MINIMR_DNS_QTYPE_ZONEMD      63
-
-//#define MINIMR_DNS_QUERY_TYPE(_qname_) \
-//    struct { \
-//        uint16_t qtype; \
-//        uint16_t unicast_qclass; \
-//        uint16_t qname_length; \
-//        uint8_t qname[sizeof(_qname_)] \
-//    }
-//
-//#define MINIMR_DNS_QUERY_INIT(_qtype_, _unicast_qclass_, _qname_) \
-//    { \
-//        .qtype = _qtype_, \
-//        .unicast_qclass = _unicast_qclass_, \
-//        .qname_length = sizeof(_qname_), \
-//        .qname = _qname_ \
-//    }
+// likely irrelevant
+//#define MINIMR_DNS_TYPE_AFSDB       18
+//#define MINIMR_DNS_TYPE_APL         42
+//#define MINIMR_DNS_TYPE_CAA         257
+//#define MINIMR_DNS_TYPE_CDNSKEY     60
+//#define MINIMR_DNS_TYPE_CDS         59
+//#define MINIMR_DNS_TYPE_CERT        37
+//#define MINIMR_DNS_TYPE_CNAME       5
+//#define MINIMR_DNS_TYPE_CSYNC       62
+//#define MINIMR_DNS_TYPE_DHCID       49
+//#define MINIMR_DNS_TYPE_DLV         32769
+//#define MINIMR_DNS_TYPE_DNAME       39
+//#define MINIMR_DNS_TYPE_DNSKEY      48
+//#define MINIMR_DNS_TYPE_DS          43
+//#define MINIMR_DNS_TYPE_HINFO       13
+//#define MINIMR_DNS_TYPE_HIP         55
+//#define MINIMR_DNS_TYPE_IPSECKEY    45
+//#define MINIMR_DNS_TYPE_KEY         25
+//#define MINIMR_DNS_TYPE_KX          36
+//#define MINIMR_DNS_TYPE_LOC         29
+//#define MINIMR_DNS_TYPE_MX          15
+//#define MINIMR_DNS_TYPE_NAPTR       35
+//#define MINIMR_DNS_TYPE_NS          2
+//#define MINIMR_DNS_TYPE_NSEC        47
+//#define MINIMR_DNS_TYPE_NSEC3       50
+//#define MINIMR_DNS_TYPE_NSEC3PARAM  51
+//#define MINIMR_DNS_TYPE_OPENGPGKEY  61
+//#define MINIMR_DNS_TYPE_RRSIG       46
+//#define MINIMR_DNS_TYPE_RP          17
+//#define MINIMR_DNS_TYPE_SIG         24
+//#define MINIMR_DNS_TYPE_SMIMEA      53
+//#define MINIMR_DNS_TYPE_SOA         6
+//#define MINIMR_DNS_TYPE_SSHFP       44
+//#define MINIMR_DNS_TYPE_TA          32768
+//#define MINIMR_DNS_TYPE_TKEY        249
+//#define MINIMR_DNS_TYPE_TLSA        52
+//#define MINIMR_DNS_TYPE_TSIG        250
+//#define MINIMR_DNS_TYPE_URI         256
+//#define MINIMR_DNS_TYPE_ZONEMD      63
 
 struct minimr_dns_query_stat {
     uint16_t type;
     uint16_t unicast_class;
 
     uint16_t name_length;
-    uint8_t * name;
-};
 
-#define MINIMR_DNS_NEXT_QUERY(qname_length) (qname_length + 6)
+    uint16_t name_offset; // used for compressed name lookups
+    uint16_t ir; // record index of matched record (used in processing to avoid reprocessing)
+    uint8_t relevant; //
+};
 
 #define MINIMR_DNS_CACHEFLUSH   0x8000  // cache flush requested
 #define MINIMR_DNS_RRCLASS      0x07ff
 
-#define MINIMR_DNS_RRTYPE_
+#define MINIMR_DNS_COMPRESSED_NAME          0xc0
+#define MINIMR_DNS_COMPRESSED_NAME_OFFSET    0x3f
 
-
-struct minimr_dns_rr {
+struct minimr_dns_rr_stat {
     uint16_t type;
     uint16_t cache_class;
     uint32_t ttl;
     uint16_t dlength;
 
     uint16_t name_length;
+    uint16_t name_offset;
+
+    uint16_t data_offset;
+};
+
+
+enum minimr_dns_rr_fun_type {
+    minimr_dns_rr_fun_type_is_uptodate,
+    minimr_dns_rr_fun_type_get_answer_rrs,
+    minimr_dns_rr_fun_type_get_authority_rrs,
+    minimr_dns_rr_fun_type_get_additional_rrs
+};
+
+typedef int (*minimr_dns_rr_fun)( enum minimr_dns_rr_fun_type fun, ... );
+
+struct minimr_dns_rr {
+    uint16_t type;
+    uint16_t cache_class;
+    uint32_t ttl;
+
+    minimr_dns_rr_fun fun;
+
+    uint16_t name_length;
 
     uint8_t name[];
 };
+
+// identical memory layout as struct minimr_dns_rr
+#define MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    struct { \
+        uint16_t type; \
+        uint16_t cache_class; \
+        uint32_t ttl; \
+        \
+        minimr_dns_rr_fun fun; \
+        \
+        uint16_t name_length; \
+        \
+        uint8_t name[__namelen__];
+
+#define MINIMR_DNS_RR_TYPE_BODY_A() \
+        uint8_t ip[4];
+
+#define MINIMR_DNS_RR_TYPE_BODY_AAAA() \
+        uint16_t ip[8];
+
+#define MINIMR_DNS_RR_TYPE_BODY_PTR(__domainlen__) \
+        uint8_t domain[__domainlen__];
+
+
+#define MINIMR_DNS_RR_TYPE_BODY_SRV(__targetlen__) \
+        uint16_t priority; \
+        uint16_t weight; \
+        uint16_t port; \
+        uint8_t target[__targetlen__];
+
+
+#define MINIMR_DNS_RR_TYPE_BODY_TXT(__txtlen__) \
+        uint8_t txt[__txtlen__];
+
+
+#define MINIMR_DNS_RR_TYPE_END() \
+    }
+
+#define MINIMR_DNS_RR_TYPE_A(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BODY_A() \
+    MINIMR_DNS_RR_TYPE_END()
+
+#define MINIMR_DNS_RR_TYPE_AAAA(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BODY_AAAA() \
+    MINIMR_DNS_RR_TYPE_END()
+
+#define MINIMR_DNS_RR_TYPE_PTR(__namelen__, __domainlen__) \
+    MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BODY_PTR( __domainlen__) \
+    MINIMR_DNS_RR_TYPE_END()
+
+#define MINIMR_DNS_RR_TYPE_SRV(__namelen__, __targetlen__) \
+    MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BODY_SRV(__targetlen__) \
+    MINIMR_DNS_RR_TYPE_END()
+
+#define MINIMR_DNS_RR_TYPE_TXT(__namelen__, __txtlen__) \
+    MINIMR_DNS_RR_TYPE_BEGIN(__namelen__) \
+    MINIMR_DNS_RR_TYPE_BODY_TXT(__txtlen__) \
+    MINIMR_DNS_RR_TYPE_END()
+
+//#define MINIMR_DNS_RR_INIT(__name__, __type__, __cache_class__, __ttl__, __user_data__, __fun__) \
+//    { \
+//        .type = __type__, \
+//        .cache_class = __cache_class__, \
+//        .ttl = __ttl__, \
+//        .name_length = sizeof(__name__) - 1, \
+//        .use_data = __user_data__, \
+//        .fun = __fun__, \
+//        .name = __name__ \
+//    }
 
 
 inline void minimr_dns_ntoh_hdr(struct minimr_dns_hdr * hdr, uint8_t * bytes)
@@ -188,86 +278,16 @@ inline void minimr_dns_hton_hdr(uint8_t * bytes, struct minimr_dns_hdr * hdr)
     bytes[11] = hdr->nextrarr & 0xff;
 }
 
-uint8_t minimr_dns_stat_query(struct minimr_dns_query_stat *stat, uint8_t * bytes, uint16_t maxlength)
-{
-    // minlen = QNAME(1)[= "\0"] QTYPE(2) QCLASS(2)
-    if (maxlen <= 5){
-        return MINIMR_NOT_OK;
-    }
-    uint16_t length = 0;
+void minimr_dns_normalize_name(struct minimr_dns_rr * rr);
 
-    for(; length < maxlen && bytes[length] != '\0'; length++){
-        // just looking for the end of qname
-        // note: it's not checked wether the qname is correctly formatted
-    }
+uint8_t minimr_dns_extract_query_stat(struct minimr_dns_query_stat * stat, uint8_t * msg, uint16_t * pos, uint16_t msglen);
+uint8_t minimr_dns_extract_rr_stat(struct minimr_dns_rr_stat * stat, uint8_t * msg, uint16_t *pos, uint16_t msglen);
 
-    if (length == 0 || bytes[length] != '\0' || length + 4 > maxlength){
-        return MINIMR_NOT_OK;
-    }
-
-    stat->qtype = (bytes[length++] << 8) | bytes[length++];
-    stat->unicast_qclass = (bytes[length++] << 8) | bytes[length++];
-
-    stat->qname_length = length;
-    stat->qname = bytes;
-
-    return MINIMR_OK;
-}
-
-//#define MINIMR_DNS_MAX_QUERIES 1
-
-// TODO
-void minimr_handle_msg(uint8_t * bytes, uint16_t length){
-
-    if (length < MINIMR_DNS_HDR_SIZE){
-        return;
-    }
-
-    struct minimr_dns_hdr hdr;
-
-    minimr_dns_ntoh_hdr(&hdr, bytes);
-
-    // no questions? nothing to do!
-    if (hdr.nquestions == 0){
-        return;
-    }
-
-    uint16_t len = MINIMR_DNS_HDR_SIZE;
-
-    for(uint16_t qi = 0; qi < hdr.nquestions; qi++){
-
-        struct minimr_dns_query_stat stat;
-
-        uint8_t res = minimr_dns_stat_query(&stat, &bytes[len], length - len);
-
-        if (res == MINIMR_NOT_OK){
-            // we could respond that it was a faulty query..
-            return;
-        }
-
-        for(uint16_t ri = 0; ri < 1; ri++){
-            if (stat.type != rr[ri].type) continue;
-            if (stat.unicast_class && MINIMR_DNS_QCLASS != rr[ri].cache_class && MINIMR_DNS_RRCLASS) continue;
-
-            if (stat.name_length != rr[ri].name_length) continue;
-
-            // pretty much a reverse memcmp
-            // reverse order would in principle allow for partials..
-            uint8_t stop = 0;
-            for(int32_t i = stat.name_length - 1; stop == 0 && i > 0; i--){
-                if (stat.name[i] != rr[ri].name[i]){
-                    stop = 1;
-                }
-            }
-
-            if (stop) continue;
-
-            // so it's a match and we might consider responding
-            // but before we should also check if our response is already known
-        }
-
-    }
-
-}
+uint8_t minimr_handle_msg(
+        uint8_t * msg, uint16_t msglen,
+        struct minimr_dns_query_stat stats[], uint16_t nqstats,
+        struct minimr_dns_rr ** records, uint16_t nrecords,
+        uint8_t *outmsg, uint16_t * outmsglen, uint16_t outmsgmaxlen
+);
 
 #endif //MINIMR_DNS_MINIMR_DNS_H
