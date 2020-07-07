524929cc3d8c82d9843757688d75b8b2ac632d87
philip <me@filou.se>
2020-07-07 15:39:24 +0200

    license note

--- minimr.h
+++ minimr.h
@@ -1,8 +1,30 @@
-//
-// Created by Philip Tschiemer on 24.06.20.
-//
-// official definitions that most likely are not needed are just commented out
-//
+/**
+ * minimr - mini mDNS Responder (framework)
+ *
+ * https://github.com/tschiemer/minimr
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Philip Tschiemer, filou.se
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef MINIMR_MINIMR_H
 #define MINIMR_MINIMR_H
